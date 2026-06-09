fa74f1a1f653d388b38aff1e069170fee7c004cb
Himadri Bhardwaj <himadribhardwaj56@gmail.com>
2026-06-09 20:59:20 +0530

    Starting Again

--- src/fix-imports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Fix imports in all JS files
-find src -name "*.js" -type f -exec sed -i '' 's/from '\''\.\/components\/\([^'\'']*\)\.jsx'\''/from '\''\.\/components\/\1'\''/g' {} +
-find src -name "*.js" -type f -exec sed -i '' 's/from '\''\.\/pages\/\([^'\'']*\)\.jsx'\''/from '\''\.\/pages\/\1'\''/g' {} +
-find src -name "*.js" -type f -exec sed -i '' 's/from '\''\.\/\([^'\'']*\)\.jsx'\''/from '\''\.\/\1'\''/g' {} +
-
-echo "Imports fixed!"
